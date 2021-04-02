8e5950f54fb5749bacd8ee0b58635f3ac7b1e595
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-04-02 23:06:32 +0200

    Added picom config and layout script

---
+++ .config/layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --primary --mode 1920x1080 --pos 1080x373 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left --output DP-0 --off --output DP-1 --off --output DVI-D-0 --mode 1920x1080 --pos 3000x0 --rotate right --output VGA-1-1 --off --output HDMI-1-2 --off --output DP-1-2 --off --output HDMI-1-3 --off --output HDMI-1-4 --off
